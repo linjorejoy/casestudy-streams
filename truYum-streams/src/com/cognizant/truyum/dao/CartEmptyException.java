f6b12e0083469efec90c60942451dd19e5a06a51
Linjo Rejoy <52414609+linjorejoy@users.noreply.github.com>
2020-10-24 15:10:25 +0530

    init commit

---
+++ truYum-streams/src/com/cognizant/truyum/dao/CartEmptyException.java
@@ -0,0 +1,14 @@
+package com.cognizant.truyum.dao;
+
+public class CartEmptyException extends Exception {
+	// private String message;
+
+	public CartEmptyException() {
+		super();
+	}
+
+	// @Override
+	// public String getMessage() {
+	// return message;
+	// }
+}
